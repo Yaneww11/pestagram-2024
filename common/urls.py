fc223b8a732dcabc1631605200bf13fd84d97b5e
Yane <yane.yanev@pytek.bg>
2024-11-09 19:03:18 +0200

    like functionality

--- common/urls.py
+++ common/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from common import views
 
 urlpatterns = [
-    path('', views.home_page, name='home-page')
+    path('', views.home_page, name='home-page'),
+    path('like/<int:photo_id>/', views.like_functionality, name='like'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from common.models import Like
+from photos.models import Photo
+
 
 def home_page(request):
-    context = {'username' : 'Yane'}
+    all_photos = Photo.objects.all()
+
+    context = {
+        'username' : 'Yane',
+        'all_photos': all_photos
+    }
     return render(request, 'common/home-page.html', context)
+
+def like_functionality(request, photo_id: int):
+    liked_object = Like.objects.filter(
+        to_photo_id=photo_id
+    ).first()
+
+    if liked_object:
+        liked_object.delete()
+    else:
+        like = Like(to_photo_id=photo_id)
+        like.save()
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#{photo_id}')
